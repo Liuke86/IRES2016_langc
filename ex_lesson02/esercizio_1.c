bfad92cb1aba1a630711391f00e42de1a44ec4b8
Liuke86 <gokuangel@libero.it>
2016-02-08 10:09:28 +0100

    Primo commit

---
+++ ex_lesson02/esercizio_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+   //creare array 20 elementi
+   int myarrey[20];
+   myarrey[0] = 20;
+   myarrey[1] = 19;
+
+   for(i = 0; i < 20; i++)
+   {
+      printf("%d", i);
+
+   }
+
+}
